74873c58c6cc1a95fd2f2385951423c2bc2c1bf8
EliDevToBe <maxime.ktr@proton.me>
2024-06-25 11:04:08 +0200

    added some examples of interactions with Express Server

--- serveur_api/serveurtest.js
+++ serveur_api/serveurtest.js
@@ -1,13 +1,41 @@
 const express = require('express')
 const app = express()
 
-app.get("/",
+app.get("/doc",
     function (requete, reponse) {
-        reponse.send('Bienvenue sur le site Schlechte Mobel!')
+        reponse.send("si /persons alors json")
     }
 )
 
-app.listen(9090, function() {
+// ==== Ecoute la request GET sur la route /persons
+app.get("/persons", (request, response) => {
+
+    response.json({
+        result: [
+            {
+                name: "Jack",
+                height: "180"
+            },
+            {
+                name: "John",
+                height: "180"
+            },
+            {
+                name: "Jane",
+                height: "176"
+            }]
+    })
+})
+
+// ==== Ecoute la request POST sur la route /userRegistration
+app.post("/userregistration", (request, response) => {
+    // fait les request pour remplir la DB et renvoi OK*
+    response.sendStatus(200);
+    response.end();
+})
+
+// ==== Lance le server
+app.listen(9090, function () {
     console.log('Mon serveur écoute sur le port 9090')
-    
+
 })
