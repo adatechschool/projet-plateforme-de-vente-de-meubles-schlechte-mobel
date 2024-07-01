678c54f32d83f1f840b0f14085fc42ea251c78d7
Katshual <139873320+Katshual@users.noreply.github.com>
2024-07-01 11:07:12 +0200

    ajout fonction addDatabase

---
+++ serveur_api/index.js
@@ -0,0 +1,79 @@
+require("dotenv").config();
+const pg = require("pg")
+const { Pool, Client } = pg
+
+// ==== This takes the connectionString from the .env config file
+const connectionString = process.env.DATABASE_URL;
+
+// ==== Opening a pool of 'clients'
+const pool = new Pool({
+    connectionString
+});
+// ==== Checking errors when idle
+pool.on("error", (err, client) => {
+    console.error("Error on idle client", err);
+    process.exit(-1);
+})
+
+// ==== Connection as a single Client
+// == The 'SELECT NOW()' query is basic to test a connection
+// == even without any tables in DB
+async function connection() {
+
+    const client = new Client({
+        connectionString,
+    })
+    await client.connect();
+
+    const requestResult = await client.query('SELECT * from test');
+    console.log(requestResult.rows)
+
+    await client.end();
+}
+connection();
+
+module.exports={addToDatabase} 
+async function addToDatabase(name, surname) {
+    const client= new Client({
+        connectionString,
+    });
+
+try {
+    await client.connect();
+    //ajouter une nouvelle ligne à la  table
+    const result=await client.query(
+        'INSERT INTO test (name, surname) VALUES ($1,$2)',
+        [name, surname]
+    );
+    console.log(`Ajouté ${result.rowCount} ligne(s)`)
+    await client.end();
+    
+} catch (error) {
+    console.log('error')
+}
+
+
+}
+//addToDatabase()
+
+// ==== Connecting with a pool for a single query
+// == Let's the pool open
+async function singlePoolQuery() {
+
+    const response = await pool.query('SELECT NOW()');
+    console.log(response.rows);
+}
+singlePoolQuery();
+
+// ==== Connecting with a Client from a Pool, use it,
+// == then return the Client to the Pool to be ready for next use
+async function queryWithPool() {
+
+    const client = await pool.connect();
+    const response = await client.query('SELECT * from test');
+    console.log(response.rows);
+
+    // Release the Client we took from Pool
+    client.release();
+}
+// queryWithPool();
